7c8bc1bedf20e466900333790e7000ea96bc07a2
Toan Le <toanle050196@gmail.com>
2026-04-23 23:18:58 +0700

    Fix responsive

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import en from '../i18n/en'
 import vi from '../i18n/vi'
 
@@ -12,6 +12,11 @@ export function LanguageProvider({ children }) {
   const t = translations[language]
 
   const toggleLanguage = () => setLanguage(lang => lang === 'en' ? 'vi' : 'en')
+
+  useEffect(() => {
+    document.documentElement.lang = language
+    document.documentElement.setAttribute('data-lang', language)
+  }, [language])
 
   return (
     <LanguageContext.Provider value={{ language, toggleLanguage, t }}>
